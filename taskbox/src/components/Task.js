261a27e71639ec17c1ee98a4e885e56b1cd76baa
yhorie <yuk.horie@gmail.com>
2020-02-16 07:03:26 +0900

    Finish 'Build out the states'

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-export default function Task ({ task: { id, title, state }, onArchiveTask, onPinTask}) {
+export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+  /*
   return (
     <div className="list-item">
       <input type="text" value={title} readOnly={true}/>
+    </div>
+  )
+  */
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={'icon-star'} />
+          </a>
+        )}
+      </div>
     </div>
   )
 }
